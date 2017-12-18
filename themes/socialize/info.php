a63b12ab1ef0f60012d88e98051a73ea643788b9
Pegasus <iblispega@yahoo.com>
2017-12-18 19:35:10 +0800

    Clone default theme
    Create a clone theme

---
+++ themes/socialize/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'Socialize';
+
+$themeFolder = 'socialize';
+
+$themeAuthor = 'Bluecozza';
+
+$themeAuthorUrl = 'mailto:wowondersocial@gmail.com';
+
+$themeVirsion = '1';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
