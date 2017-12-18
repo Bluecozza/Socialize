626d6d8476fff6f8b409a7140af8517a194fbf0c
Pegasus <iblispega@yahoo.com>
2017-12-18 19:42:13 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,25 +1,25 @@
-<?php
-// +------------------------------------------------------------------------+
-// | @author Deen Doughouz (DoughouzForest)
-// | @author_url 1: http://www.wowonder.com
-// | @author_url 2: http://codecanyon.net/user/doughouzforest
 // | @author_email: wowondersocial@gmail.com   
 // +------------------------------------------------------------------------+
 // | WoWonder - The Ultimate PHP Social Networking Platform
-// | Copyright (c) 2016 WoWonder. All rights reserved.
+// | Copyright (c) 2017 WoWonder. All rights reserved.
 // +------------------------------------------------------------------------+
+@header('Location: install');
+exit();
 // MySQL Hostname
-$sql_db_host = "localhost";
+$sql_db_host = "db_name";
+
 // MySQL Database User
-$sql_db_user = "root";
+$sql_db_user = "db_user";
+
 // MySQL Database Password
-$sql_db_pass = "";
+$sql_db_pass = "db_pass";
+
 // MySQL Database Name
-$sql_db_name = "wowonder";
+$sql_db_name = "db_name";
+
 
 // Site URL
-$site_url = "http://localhost/socialize"; // e.g (http://example.com)
+$site_url = "site_url"; // e.g (http://example.com)
 
-// Purchase code
-$purchase_code = "123"; // Your purchase code, don't give it to anyone. 
+
 ?>
